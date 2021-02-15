f7114a0c3b101a37dbff8087f23fcb74a623b3e8
Vitali <vitshu1999@gmail.com>
2021-02-15 15:52:05 +0300

    Добавлен метод расширение для извлечения userid

---
+++ FilmsCatalog/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace FilmsCatalog.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string UserId(this ClaimsPrincipal principal) => principal.Claims
+            .Union(principal.Identities.SelectMany(x => x.Claims))
+            .FirstOrDefault(x => x.Type.Equals(ClaimTypes.NameIdentifier))?.Value;
+    }
+}
